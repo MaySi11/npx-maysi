ed51e8cc01495e8e07cacb45900cba313309b7b1
MaySi11 <mayanksaini78@gmail.com>
2023-10-24 16:11:22 +0530

    My npx Card!

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# npx-maysi
-My NPX Social Card
+## It's me MaySi! <img src="https://media.giphy.com/media/hvRJCLFzcasrR4ia7z/giphy.gif" width="30px">
+
+This is my NPX card to connect with me directly via console or terminal
+
+## Usage
+
+Just hit, and get know me in your terminal 👇
+
+```sh
+npx maysi
+```
+
+Not including how to use it globally because I'm not sure why you'd want this as a global command. That'd be creepy.
---
+++ card.js
@@ -0,0 +1,56 @@
+#!/usr/bin/env node
+
+// Modules
+import boxen from "boxen";
+import chalk from "chalk";
+
+const data = {
+  name: chalk.red("                  Mayank Saini"),
+  about: `${chalk.white("Software Engineer in making at ")} ${chalk.cyan(
+    "LNMIIT"
+  )}`,
+  github: chalk.gray("https://github.com/") + chalk.blue("MaySi11"),
+  twitter: chalk.gray("https://twitter.com/") + chalk.cyanBright("May_Si_"),
+  linkedin:
+    chalk.gray("https://linkedin.com/in/") + chalk.green("mayank-s-099b75201"),
+  web: chalk.cyan("https://anoroc.win/"),
+
+  npx: chalk.red("npx maysi"),
+
+  labelWork: "         ",
+  labelTwitter: chalk.white.bold("    Twitter:"),
+  labelGitHub: chalk.white.bold("     GitHub:"),
+  labelLinkedIn: chalk.white.bold("   LinkedIn:"),
+  labelWeb: chalk.white.bold("        Web:"),
+  labelCard: chalk.white.bold("       Card:"),
+};
+
+const me = boxen(
+  [
+    `${data.name}`,
+    ``,
+    `${data.labelWork}  ${data.about}`,
+    ``,
+    `${data.labelGitHub}  ${data.github}`,
+    `${data.labelTwitter}  ${data.twitter}`,
+    `${data.labelLinkedIn}  ${data.linkedin}`,
+    `${data.labelWeb}  ${data.web}`,
+    ``,
+    `${data.labelCard}  ${data.npx}`,
+    ``,
+
+    `${chalk.italic("    My DMs is always open, Whether you have a")}`,
+    `${chalk.italic(
+      "    question or just want talk about Tech, Music or Anime! "
+    )}`,
+  ].join("\n"),
+  {
+    margin: 1,
+    float: "center",
+    padding: 1,
+    borderStyle: "bold",
+    borderColor: "magenta",
+  }
+);
+
+console.log(me);
